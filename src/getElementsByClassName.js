f7e0e375f53c2058edcb292cd80f41ac1d2ff1dc
ejk91 <enochkim91@gmail.com>
2017-02-06 11:59:20 -0800

    implemented getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  // create storage results 
+  var results = [];
+  node = node || document.body; 
+
+  // develop base case conditional flag - parent
+  if(node.classList) { //checks if ClassList exists
+    if(node.classList.contains(className)) { //if classList has className
+        results.push(node); // if matched, push to storage results
+    }
+  }
+
+  // recursively call on children
+  for(var i=0; i<node.children.length; i++) {
+    var childList = getElementsByClassName(className, node.children[i]);
+    results = results.concat(childList);
+  }
+
+// return results 
+    return results;
+
 };
