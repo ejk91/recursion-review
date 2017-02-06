24e50600a296cdcc526fc5a9e6f2f4975790acc7
ejk91 <enochkim91@gmail.com>
2017-02-06 15:22:29 -0800

    Completed Pair-Review for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,38 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  // array case
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    } else {
+      var results = [];
+      for (var i = 0; i < obj.length; i++) {
+        results.push(stringifyJSON(obj[i]));
+      }
+      return '[' + results.join(',') + ']';
+    }
+  }
+  
+//object case
+  if ( obj && typeof obj === 'object') {
+    var results = [];
+    for (var key in obj) {
+      if (obj[key] === undefined || obj[key] === function () {}) { 
+        return '{}';
+      } else {
+        results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + results.join(',') + '}';
+  }
+
+  // string base case
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  return '' + obj;
+
 };
